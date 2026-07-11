f67eb90084eb257dbf6ed37778cc362082bddbbd
jo <djh1997@hotmail.com>
2026-07-11 11:48:48 +0100

    add combo

---
+++ assets/tulip/round-about-combo.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#set variables
+OF=$@'.svg'
+echo  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">' > $OF
+echo  '<g id="arm"><line style="stroke: black; fill: none;" x1="10" y1="10" x2="10" y2="18"/></g>' >> $OF
+echo  '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="18" r="1"/>' >> $OF
+echo  '<path style="fill: none; stroke: rgb(0, 0, 0);" d="M 8 4 L 10 2 L 12 4" transform="rotate('$(($2*30))' 10 10)"></path>' >> $OF
+for i in $@;
+do
+  if [[ $i="R" ]]
+    then
+      echo 'te'$i'st'
+    else
+      echo '<use href="#arm" transform="rotate('$((($i+6)*30))' 10 10)" />' >> $OF
+      echo 'te'$i'st2'
+    fi
+done
+if [[ $1='R' ]]
+  then
+    echo '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="10" r="3"/><path style="fill: none; stroke: rgb(255, 255, 255);" d="M 10 10 L 13 13"/></svg>' >> $OF
+elif [[ $1='M' ]]
+  then
+    echo '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="10" r="1"/></svg>' >> $OF
+  fi
