6d6f934e4b0028674a0c23ea11a32ad0c89fc598
jo <djh1997@hotmail.com>
2026-07-11 10:05:19 +0100

    update round about generator

--- assets/tulip/round-about.sh
+++ assets/tulip/round-about.sh
@@ -4,10 +4,11 @@
 #sha=$(head -c 11 sha.txt  | sha256sum)
 #OF='output/'$word'.svg'
 OF='arm.svg'
-echo  '<?xml version="1.0" encoding="utf-8"?><svg height="190" width="162" xmlns="http://www.w3.org/2000/svg">' > $OF
-echo  '<g id="arm"><line style="stroke: black; fill: none;" x1="50" y1="10" x2="50" y2="50"/><line style="stroke: black; fill: none;" x1="40" y1="10" x2="60" y2="10"/><polyline style="stroke: black; fill: none;" points="40 10 50 10 50 0" transform="rotate(45 50 10)"/></g>' >> $OF
-for i in  {0..360..10}; do
-  echo '
-  <use href="#arm" transform="rotate('$i' 50 50)" />' >> $OF
+echo  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">' > $OF
+echo  '<g id="arm"><line style="stroke: black; fill: none;" x1="10" y1="10" x2="10" y2="18"/></g>' >> $OF
+echo  '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="18" r="1"/>' >> $OF
+echo  '<path style="fill: none; stroke: rgb(0, 0, 0);" d="M 8 4 L 10 2 L 12 4" transform="rotate('$(($1*30))' 10 10)"></path>' >> $OF
+for i in $@; do
+  echo '<use href="#arm" transform="rotate('$((($i+6)*30))' 10 10)" />' >> $OF
 done
-echo '</svg>' >> $OF
+echo '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="10" r="3"/>  <path style="fill: none; stroke: rgb(255, 255, 255);" d="M 10 10 L 13 13"/></svg>' >> $OF
