ec389bb35f9197a5373302831236b2e02b5cb619
jo <djh1997@hotmail.com>
2026-07-11 10:26:33 +0100

    finalise mini and roundabout generator

--- assets/tulip/mini-round-about.sh
+++ assets/tulip/mini-round-about.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #set variables
-OF='R '$@'.svg'
+OF='M '$@'.svg'
 echo  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">' > $OF
 echo  '<g id="arm"><line style="stroke: black; fill: none;" x1="10" y1="10" x2="10" y2="18"/></g>' >> $OF
 echo  '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="18" r="1"/>' >> $OF
@@ -8,4 +8,4 @@ echo  '<path style="fill: none; stroke: rgb(0, 0, 0);" d="M 8 4 L 10 2 L 12 4" t
 for i in $@; do
   echo '<use href="#arm" transform="rotate('$((($i+6)*30))' 10 10)" />' >> $OF
 done
-echo '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="10" r="3"/><path style="fill: none; stroke: rgb(255, 255, 255);" d="M 10 10 L 13 13"/></svg>' >> $OF
+echo '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="10" r="1"/></svg>' >> $OF
