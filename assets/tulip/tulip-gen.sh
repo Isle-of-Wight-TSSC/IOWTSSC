bac70166462cacd8fe2abfe0d475f3b7dc3645e9
jo <djh1997@hotmail.com>
2026-07-17 07:51:37 +0100

    trim file

--- assets/tulip/tulip-gen.sh
+++ assets/tulip/tulip-gen.sh
@@ -26,27 +26,22 @@ case "$mode" in
     ;;
 esac
 
-echo $rotation_multiplier
-echo $rotation_offset
-
 echo '<path style="fill: none; stroke: rgb(0, 0, 0);" d="M 8 4 L 10 2 L 12 4" transform="rotate('$(($2 * rotation_multiplier))' 10 10)"></path>' >> "$OF"
-echo $(( $2 * rotation_multiplier ))
 
 for i in "${@:2}"; do
-  if [ "$i" = "R" ] || [ "$i" = "M" ] || [ "$i" = "J" ]; then
+  if [ "$i" = "R" ] || [ "$i" = "M" ] || [ "$i" = "J" ] || [ "$i" = "T" ]; then
     echo $i
   else
     echo '<use href="#arm" transform="rotate('$(((i + rotation_offset) * rotation_multiplier))' 10 10)" />' >> "$OF"
-    echo $(((i + rotation_offset) * rotation_multiplier))
   fi
 done
 
-if [ "$1" = "R" ]|| [ "$1" = "r" ]; then
+if [ "$1" = "R" ] || [ "$1" = "r" ]; then
   echo '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="10" r="3"/><path style="fill: none; stroke: rgb(255, 255, 255);" d="M 10 10 L 13 13"/></svg>' >> "$OF"
-elif [ "$1" = "M" ]|| [ "$1" = "m" ]; then
+elif [ "$1" = "M" ] || [ "$1" = "m" ]; then
   echo '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="10" r="1"/></svg>' >> "$OF"
-elif [ "$1" = "J" ]|| [ "$1" = "j" ]; then
+elif [ "$1" = "J" ] || [ "$1" = "j" ]; then
   echo '</svg>' >> "$OF"
-elif [ "$1" = "T" ]|| [ "$1" = "t" ]; then
+elif [ "$1" = "T" ] || [ "$1" = "t" ]; then
   echo '<text x="3" y="18" font-size="7">🚦</text></svg>' >> "$OF"
 fi
