def90cac3a90605d03980f4537246e4230acf1e8
jo <djh1997@hotmail.com>
2026-07-18 10:00:07 +0100

    add central circle to hide gap change colour from rgb to names

--- assets/tulip/tulip-gen.js
+++ assets/tulip/tulip-gen.js
@@ -99,19 +99,19 @@ function tulip_gen() {
 
   var text = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">';
   text +=
-    '<g id="arm"><line style="stroke: black; fill: none;" x1="10" y1="10" x2="10" y2="18"/></g>';
+    '<g id="arm"><line style="stroke: #000; fill: none;" x1="10" y1="10" x2="10" y2="18"/></g>';
   text +=
-    '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="18" r="1"/>';
+    '<circle style="stroke: #000; stroke-width: 1; fill: #FFF;" cx="10" cy="18" r="1"/>';
 
   var parsedExit = parseFloat(exitValue);
   if (!isNaN(parsedExit)) {
     text +=
-      '<path style="fill: none; stroke: rgb(0, 0, 0);" d="M 8 4 L 10 2 L 12 4" transform="rotate(' +
+      '<path style="fill: none; stroke: #000;" d="M 8 4 L 10 2 L 12 4" transform="rotate(' +
       parsedExit * rotationMultiplier +
       ' 10 10)"></path>';
   } else {
     text +=
-      '<path style="fill: none; stroke: rgb(0, 0, 0);" d="M 8 4 L 10 2 L 12 4"></path>';
+      '<path style="fill: none; stroke: #000;" d="M 8 4 L 10 2 L 12 4"></path>';
   }
 
   for (var i = 0; i < values.length; i++) {
@@ -133,14 +133,16 @@ function tulip_gen() {
 
   if (mode === "R" || mode === "r") {
     text +=
-      '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="10" r="3"/><path style="fill: none; stroke: rgb(255, 255, 255);" d="M 10 10 L 13 13"/>';
+      '<circle style="stroke: #000; stroke-width: 1; fill: #FFF;" cx="10" cy="10" r="3"/><path style="fill: none; stroke: #FFF;" d="M 10 10 L 13 13"/>';
   } else if (mode === "M" || mode === "m") {
     text +=
-      '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="10" r="1"/>';
+      '<circle style="stroke: #000; stroke-width: 1; fill: #FFF;" cx="10" cy="10" r="1"/>';
   } else if (mode === "T" || mode === "t") {
     text += '<text x="3" y="18" font-size="7">&#x1F6A6</text>';
   }
-
+if (mode === "J" || mode === "j" || mode === "T" || mode === "t") {
+    text += '<circle style="fill: #000;" cx="10" cy="10" r="0.5"/>';
+  }
   text += "</svg>";
   currentSvg = text;
   output.innerHTML = currentSvg;
