1ef1c758bad467aca8c2881bebee3cf382ff5d44
jo <djh1997@hotmail.com>
2026-07-18 09:28:55 +0100

    resize help text

--- assets/tulip/tulip-gen.js
+++ assets/tulip/tulip-gen.js
@@ -1,9 +1,15 @@
 var currentSvg = "";
-var helpText = 'Mode is one of R, M, J, T (bearing by clock position) or r, m, j, t (bearing by degrees). <br>For uppercase modes, Exit and Other exits must be numbers from 0-12. For lowercase modes, Exit and Other exits must be numbers from 0-360.<br>Other_ExitN are optional additional exits to draw.<br>For example Mode: R, Exit: 9, Others: 12 3 would generate this tulip diagram.<br><img src="R_9_12_3.svg" alt="example tulip diagram" width="20%"><br>Click "Generate" to display the tulip diagram.<br>Click "Download SVG" to download the generated diagram as an SVG file.';
+var helpText =
+  'Mode is one of R, M, J, T (bearing by clock position) or r, m, j, t (bearing by degrees). <br>For uppercase modes, Exit and Other exits must be numbers from 0-12. For lowercase modes, Exit and Other exits must be numbers from 0-360.<br>Other_ExitN are optional additional exits to draw.<br>For example Mode: R, Exit: 9, Others: 12 3 would generate this tulip diagram.<br><img src="R_9_12_3.svg" alt="example tulip diagram"><br>Click "Generate" to display the tulip diagram.<br>Click "Download SVG" to download the generated diagram as an SVG file.';
 function help() {
   var help = document.getElementById("help");
 
   help.innerHTML = helpText;
+  var exampleImg = help.querySelector("img");
+  if (exampleImg) {
+    exampleImg.style.width = "20%";
+    exampleImg.style.height = "auto";
+  }
 }
 
 function tulip_gen() {
@@ -19,16 +25,26 @@ function tulip_gen() {
 
   var errors = [];
   if (!/^[RrMmJjTt]$/.test(mode)) {
-    errors.push('Mode must be one of R, M, J, T, r, m, j, t.');
+    errors.push("Mode must be one of R, M, J, T, r, m, j, t.");
   }
 
   var minExit = 0;
-  var maxExit = (mode === mode.toUpperCase()) ? 12 : 360;
+  var maxExit = mode === mode.toUpperCase() ? 12 : 360;
   var parsedExit = parseFloat(exitValue);
   if (exitValue === "" || !isNumericToken(exitValue)) {
-    errors.push('Exit ' + exitValue + ' must be a number.');
+    errors.push("Exit " + exitValue + " must be a number.");
   } else if (parsedExit < minExit || parsedExit > maxExit) {
-    errors.push('Exit ' + exitValue + ' must be between ' + minExit + '-' + maxExit + ' for mode ' + mode + '.');
+    errors.push(
+      "Exit " +
+        exitValue +
+        " must be between " +
+        minExit +
+        "-" +
+        maxExit +
+        " for mode " +
+        mode +
+        ".",
+    );
   }
 
   var values = [];
@@ -46,7 +62,17 @@ function tulip_gen() {
       }
       var parsedOther = parseFloat(otherValue);
       if (parsedOther < minExit || parsedOther > maxExit) {
-        errors.push('Other exit "' + otherValue + '" must be between ' + minExit + '-' + maxExit + ' for mode ' + mode + '.');
+        errors.push(
+          'Other exit "' +
+            otherValue +
+            '" must be between ' +
+            minExit +
+            "-" +
+            maxExit +
+            " for mode " +
+            mode +
+            ".",
+        );
       } else {
         values.push(otherValue);
       }
@@ -54,7 +80,11 @@ function tulip_gen() {
   }
 
   if (errors.length > 0) {
-    output.innerHTML = '<span style="color: red;">' + errors.join('<br>') + '</span><br>Help: ' + helpText;
+    output.innerHTML =
+      '<span style="color: red;">' +
+      errors.join("<br>") +
+      "</span><br>Help: " +
+      helpText;
     currentSvg = "";
     return;
   }
@@ -68,14 +98,20 @@ function tulip_gen() {
   }
 
   var text = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">';
-  text += '<g id="arm"><line style="stroke: black; fill: none;" x1="10" y1="10" x2="10" y2="18"/></g>';
-  text += '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="18" r="1"/>';
+  text +=
+    '<g id="arm"><line style="stroke: black; fill: none;" x1="10" y1="10" x2="10" y2="18"/></g>';
+  text +=
+    '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="18" r="1"/>';
 
   var parsedExit = parseFloat(exitValue);
   if (!isNaN(parsedExit)) {
-    text += '<path style="fill: none; stroke: rgb(0, 0, 0);" d="M 8 4 L 10 2 L 12 4" transform="rotate(' + (parsedExit * rotationMultiplier) + ' 10 10)"></path>';
+    text +=
+      '<path style="fill: none; stroke: rgb(0, 0, 0);" d="M 8 4 L 10 2 L 12 4" transform="rotate(' +
+      parsedExit * rotationMultiplier +
+      ' 10 10)"></path>';
   } else {
-    text += '<path style="fill: none; stroke: rgb(0, 0, 0);" d="M 8 4 L 10 2 L 12 4"></path>';
+    text +=
+      '<path style="fill: none; stroke: rgb(0, 0, 0);" d="M 8 4 L 10 2 L 12 4"></path>';
   }
 
   for (var i = 0; i < values.length; i++) {
@@ -89,32 +125,48 @@ function tulip_gen() {
       continue;
     }
 
-    text += '<use href="#arm" transform="rotate(' + ((parsedValue + rotationOffset) * rotationMultiplier) + ' 10 10)" />';
+    text +=
+      '<use href="#arm" transform="rotate(' +
+      (parsedValue + rotationOffset) * rotationMultiplier +
+      ' 10 10)" />';
   }
 
   if (mode === "R" || mode === "r") {
-    text += '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="10" r="3"/><path style="fill: none; stroke: rgb(255, 255, 255);" d="M 10 10 L 13 13"/>';
+    text +=
+      '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="10" r="3"/><path style="fill: none; stroke: rgb(255, 255, 255);" d="M 10 10 L 13 13"/>';
   } else if (mode === "M" || mode === "m") {
-    text += '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="10" r="1"/>';
+    text +=
+      '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="10" r="1"/>';
   } else if (mode === "T" || mode === "t") {
     text += '<text x="3" y="18" font-size="7">&#x1F6A6</text>';
   }
 
-  text += '</svg>';
+  text += "</svg>";
   currentSvg = text;
   output.innerHTML = currentSvg;
+  var exampleImg = output.querySelector("svg");
+  if (exampleImg) {
+    exampleImg.style.width = "20%";
+    exampleImg.style.height = "auto";
+  }
 }
 
 function download_svg() {
   if (!currentSvg) {
-        output.innerHTML = 'No tulip diagram generated yet.<br>Help: ' + helpText;
-
+    output.innerHTML =
+      '<span style="color: red;">No tulip diagram generated yet.</span><br>Help: ' +
+      helpText;
+    var exampleImg = output.querySelector("img");
+    if (exampleImg) {
+      exampleImg.style.width = "20%";
+      exampleImg.style.height = "auto";
+    }
     return;
   }
 
   var form = document.getElementById("frm1");
   var mode = (form.elements.mode.value || "tulip").trim();
-  var exitValue = (form.elements.exit.value || "").trim(); 
+  var exitValue = (form.elements.exit.value || "").trim();
   var othersValue = (form.elements.others.value || "").trim();
   var values = [];
   if (othersValue !== "") {
