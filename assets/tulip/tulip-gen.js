1efb7772645b27e7fac76658445e13e8f0fc600d
jo <djh1997@hotmail.com>
2026-07-20 15:36:56 +0100

    add boxy link

--- assets/tulip/tulip-gen.js
+++ assets/tulip/tulip-gen.js
@@ -9,7 +9,7 @@ var helpText =
   '<img src="R_9_12_3.svg" alt="example tulip diagram"><br>'+
   'Click "Generate" to display the tulip diagram.<br>'+
   'Click "Download SVG" to download the generated diagram as an SVG file.<br>'+
-  'For more complex junctions use <a target="_blank" href="https://www.danbp.org/tulipcomposer/tulipcomposer.html">TulipComposer2</a><br>';
+  'For more complex junctions use <a target="_blank" href="https://www.danbp.org/tulipcomposer/tulipcomposer.html">TulipComposer2</a> or use <a target="_blank" href="https://boxy-svg.com/app">boxy</a>and this template https://raw.githubusercontent.com/Isle-of-Wight-TSSC/IOWTSSC/refs/heads/main/assets/tulip/template.svg<br>';
 function help() {
   var help = document.getElementById("help");
 
