51eeaefde370f45b4e3ef3d7ebf2168267152cb4
jo <djh1997@hotmail.com>
2026-07-21 19:22:41 +0100

    improve svg optimisation to make them more light-weight

--- assets/tulip/tulip-gen.sh
+++ assets/tulip/tulip-gen.sh
@@ -14,7 +14,7 @@ OF=$(echo $OF | head -c $((${#OF}-1)))
 OF+='.svg'
 echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">' > "$OF"
 echo '<g id="arm"><line style="stroke: black; fill: none;" x1="10" y1="10" x2="10" y2="18"/></g>' >> "$OF"
-echo '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="18" r="1"/>' >> "$OF"
+echo '<circle style="stroke: black; stroke-width: 1; fill: white;" cx="10" cy="18" r="1"/>' >> "$OF"
 
 mode=$1
 rotation_multiplier=1
@@ -32,7 +32,7 @@ case "$mode" in
     ;;
 esac
 
-echo '<path style="fill: none; stroke: rgb(0, 0, 0);" d="M 8 4 L 10 2 L 12 4" transform="rotate('$(($2 * rotation_multiplier))' 10 10)"></path>' >> "$OF"
+echo '<path style="fill: none; stroke: black;" d="M 8 4 L 10 2 L 12 4" transform="rotate('$(($2 * rotation_multiplier))' 10 10)"></path>' >> "$OF"
 
 for i in "${@:2}"; do
   if [ "$i" = "R" ] || [ "$i" = "M" ] || [ "$i" = "J" ] || [ "$i" = "T" ]; then
@@ -43,9 +43,9 @@ for i in "${@:2}"; do
 done
 
 if [ "$1" = "R" ] || [ "$1" = "r" ]; then
-  echo '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="10" r="3"/><path style="fill: none; stroke: rgb(255, 255, 255);" d="M 10 10 L 13 13"/></svg>' >> "$OF"
+  echo '<circle style="stroke: black; stroke-width: 1; fill: white;" cx="10" cy="10" r="3"/><path style="fill: none; stroke: white;" d="M 10 10 L 13 13"/></svg>' >> "$OF"
 elif [ "$1" = "M" ] || [ "$1" = "m" ]; then
-  echo '<circle style="stroke: rgb(0, 0, 0); stroke-width: 1; fill: white;" cx="10" cy="10" r="1"/></svg>' >> "$OF"
+  echo '<circle style="stroke: black; stroke-width: 1; fill: white;" cx="10" cy="10" r="1"/></svg>' >> "$OF"
 elif [ "$1" = "J" ] || [ "$1" = "j" ]; then
   echo '</svg>' >> "$OF"
 elif [ "$1" = "T" ] || [ "$1" = "t" ]; then
